56ddb7f6529af83935ce5cdc7f3e246518421737
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2018-12-24 14:28:54 -0500

    cleanup of login action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -12,6 +12,7 @@ import {
 
 const API_SERVER = "http://localhost:5000";
 
+// Login - get user token
 export function loginAction(userData) {
   return {
     [RSAA]: {
@@ -53,31 +54,6 @@ export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/api/users/register", userData)
     .then(res => history.push("/login"))
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
-};
-
-// Login - get user token
-export const loginUser = userData => dispatch => {
-  axios
-    .post("/api/users/login", userData)
-    .then(res => {
-      // Save to localStorage
-
-      // Set token to localStorage
-      const { token } = res.data;
-      localStorage.setItem("jwtToken", token);
-      // Set token to Auth header
-      setAuthToken(token);
-      // Decode token to get user data
-      const decoded = jwt_decode(token);
-      // Set current user
-      dispatch(setCurrentUser(decoded));
-    })
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
