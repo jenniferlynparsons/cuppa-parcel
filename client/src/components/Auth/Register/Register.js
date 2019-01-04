d6038052d1a01b9afec34248eb065e808e141db4
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2019-01-04 15:32:33 -0500

    basic cleanup, first heroku test

--- client/src/components/Auth/Register/Register.js
+++ client/src/components/Auth/Register/Register.js
@@ -52,95 +52,83 @@ class Register extends Component {
     const { errors } = this.state;
 
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col s8 offset-s2">
-            <Link to="/" className="btn-flat waves-effect">
-              <i className="material-icons left">keyboard_backspace</i> Back to
-              home
-            </Link>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Register</b> below
-              </h4>
-              <p className="grey-text text-darken-1">
-                Already have an account? <Link to="/login">Log in</Link>
-              </p>
+      <div className="container content">
+        <Link to="/">Back to home</Link>
+        <h1>Register</h1>
+        <p>
+          Already have an account? <Link to="/login">Log in</Link>
+        </p>
+        <form noValidate onSubmit={this.onSubmit}>
+          <div className="field">
+            <label className="label">Name</label>
+            <div className="control">
+              <input
+                onChange={this.onChange}
+                value={this.state.name}
+                error={errors.name}
+                id="name"
+                type="text"
+                className={classnames("input", {
+                  invalid: "input is-danger"
+                })}
+              />
             </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.name}
-                  error={errors.name}
-                  id="name"
-                  type="text"
-                  className={classnames("", {
-                    invalid: errors.name
-                  })}
-                />
-                <label htmlFor="name">Name</label>
-                <span className="red-text">{errors.name}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
-                  className={classnames("", {
-                    invalid: errors.email
-                  })}
-                />
-                <label htmlFor="email">Email</label>
-                <span className="red-text">{errors.email}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password
-                  })}
-                />
-                <label htmlFor="password">Password</label>
-                <span className="red-text">{errors.password}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password2}
-                  error={errors.password2}
-                  id="password2"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password2
-                  })}
-                />
-                <label htmlFor="password2">Confirm Password</label>
-                <span className="red-text">{errors.password2}</span>
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem"
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Sign up
-                </button>
-              </div>
-            </form>
+            <span className="help is-danger">{errors.name}</span>
           </div>
-        </div>
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input
+                onChange={this.onChange}
+                value={this.state.email}
+                error={errors.email}
+                id="email"
+                type="email"
+                className={classnames("input", {
+                  invalid: "input is-danger"
+                })}
+              />
+            </div>
+            <span className="help is-danger">{errors.email}</span>
+          </div>
+          <div className="field">
+            <label className="label">Password</label>
+            <div className="control">
+              <input
+                onChange={this.onChange}
+                value={this.state.password}
+                error={errors.password}
+                id="password"
+                type="password"
+                className={classnames("input", {
+                  invalid: "input is-danger"
+                })}
+              />
+            </div>
+            <span className="help is-danger">{errors.password}</span>
+          </div>
+          <div className="field">
+            <label className="label">Confirm Password</label>
+            <div className="control">
+              <input
+                onChange={this.onChange}
+                value={this.state.password2}
+                error={errors.password2}
+                id="password2"
+                type="password"
+                className={classnames("input", {
+                  invalid: "input is-danger"
+                })}
+              />
+            </div>
+            <span className="help is-danger">{errors.password2}</span>
+          </div>
+          <div className="field">
+            <button type="submit" className="button is-primary">
+              Sign up
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -4,26 +4,46 @@ import { Link } from "react-router-dom";
 class Landing extends Component {
   render() {
     return (
-      <div className="container valign-wrapper">
-        <div className="row">
-          <div className="">
-            <h4>
-              <b>Build</b> a login/auth app with the <span>MERN</span> stack
-              from scratch
-            </h4>
-            <p className="flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
-            </p>
-            <br />
-            <Link to="/register" className="button is-primary">
-              Register
-            </Link>
-            <Link to="/login" className="button is-primary">
-              Log In
-            </Link>
-          </div>
+      <div className="container content">
+        <h1>Welcome to Cuppa!</h1>
+        <p>Organize your tea so you can enjoy it</p>
+
+        <div className="box">
+          <h3>Thanks for checking out Cuppa!</h3>
+          <p>
+            This project is under development and not meant for folks to use
+            yet.
+          </p>
+          <p>
+            Currently the teas added are not tied to a particular user and all
+            the data may be wiped at any time.
+          </p>
+          <p>
+            You're free to register an account, but that might get wiped, too.
+          </p>
+          <p>
+            Basically, everything is volatile on this site and you shouldn't
+            depend on it to actually store your teas yet.
+          </p>
+          <h4>
+            Follow the project on{" "}
+            <a
+              href="https://github.com/jenniferlynparsons/cuppa"
+              nofollow
+              target="_blank"
+            >
+              Github
+            </a>
+          </h4>
         </div>
+        <p>
+          <Link to="/register" className="button is-primary">
+            Register
+          </Link>
+          <Link to="/login" className="button is-primary">
+            Log In
+          </Link>
+        </p>
       </div>
     );
   }
