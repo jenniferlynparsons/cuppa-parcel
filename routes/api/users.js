b10adc643b28356d94208fb35fe113d9c16ffc92
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2019-01-04 17:10:30 -0500

    heroku setup

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
+// const keys = require("../../config/keys");
 const passport = require("passport");
 
 // Load input validation
@@ -86,7 +86,7 @@ router.post("/login", (req, res) => {
         // Sign token
         jwt.sign(
           payload,
-          keys.secretOrKey,
+          "secret",
           {
             expiresIn: 31556926 // 1 year in seconds
           },
