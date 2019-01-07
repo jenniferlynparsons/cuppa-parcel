595e38c1a41304983f6db9602f1823f7e82d27cc
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2019-01-07 17:29:13 -0500

    hmm. trying this.

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -44697,7 +44697,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35141" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41305" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
