73931f9676fd947bc64d80e7360a457918aa086c
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2019-01-08 17:19:30 -0500

    update build for heroku deploy

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -44719,7 +44719,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34491" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44675" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
